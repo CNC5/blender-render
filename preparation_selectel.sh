95a0b83203a987cf4f50575d0d6ec4ebf5f23a7c
CNC5 <67727572+CNC5@users.noreply.github.com>
2022-07-02 22:37:43 +0300

    Update preparation_selectel.sh

--- preparation_selectel.sh
+++ preparation_selectel.sh
@@ -5,8 +5,6 @@ tar -xpvf blender-3.2.0-linux-x64.tar.xz
 rm blender-3.2.0-linux-x64.tar.xz
 add-apt-repository ppa:graphics-drivers/ppa
 apt-get update
-lspci | grep -i nvidia
-gcc --version
 apt-get install linux-headers-$(uname -r)
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
 mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
@@ -15,3 +13,7 @@ dpkg -i cuda-repo-ubuntu2204-11-7-local_11.7.0-515.43.04-1_amd64.deb
 cp /var/cuda-repo-ubuntu2204-11-7-local/cuda-*-keyring.gpg /usr/share/keyrings/
 apt-get update
 apt-get install -y cuda
+mkdir -p /render/output
+cd blender-3.2.0-linux-x64
+wget https://766730.selcdn.ru/scene_storage/highway_scene.blend
+./blender -b /root/highway_scene.blend -E CYCLES -o /render/output/ -noaudio -s 1 -e 200 -a -- --cycles-device CUDA+CPU
