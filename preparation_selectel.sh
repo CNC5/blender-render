7da1eece377d48d534a0a5516e26829a91ce5e8b
CNC5 <67727572+CNC5@users.noreply.github.com>
2022-07-02 18:52:30 +0300

    Update preparation_selectel.sh

--- preparation_selectel.sh
+++ preparation_selectel.sh
@@ -1,5 +1,5 @@
-apt update
-apt install blender tmux htop awscli
+apt-get update
+apt-get install blender tmux htop awscli
 wget https://mirrors.dotsrc.org/blender/release/Blender3.2/blender-3.2.0-linux-x64.tar.xz
 tar -xpvf blender-3.2.0-linux-x64.tar.xz
 rm blender-3.2.0-linux-x64.tar.xz
@@ -8,5 +8,5 @@ mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda-repo-ubuntu2204-11-7-local_11.7.0-515.43.04-1_amd64.deb
 dpkg -i cuda-repo-ubuntu2204-11-7-local_11.7.0-515.43.04-1_amd64.deb
 cp /var/cuda-repo-ubuntu2204-11-7-local/cuda-*-keyring.gpg /usr/share/keyrings/sudo
-apt-get updatesudo
+apt-get update
 apt-get -y install cuda && reboot
