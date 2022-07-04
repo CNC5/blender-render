24f5d006db5871eb5d8fbe7e3eec838bb101db8d
CNC5 <kokfbbat@mail.ru>
2022-07-04 21:58:57 +0300

    syntax_update

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-These scripts are made for auto preparation and rendering on different rented hardware
+These scripts are made for auto preparation and rendering on different rented hardware.
 
-Last 2 words in the name are telling you for which platform the script was intended(for example *-ubuntu22-selectel.sh means that the script is supposed to be executed on ubuntu machine of version 22, rented from selectel)
+Last 2 words in the name are telling you for which platform the script was intended (for example *-ubuntu22-selectel.sh means that the script is supposed to be executed on ubuntu machine of version 22, rented from selectel).
 
-The first word is telling about the GPU that is supposed to be in use on the machine
+The first word is telling about the GPU that is supposed to be in use on the machine.
 
-Scripts require you to first load the scene in the storage container of some sort, for example of selectel - to the cloud storage container
+Scripts require you to first load the scene in the storage container of some sort, for example of selectel - to the cloud storage container.
--- nvidia-ubuntu22-selectel.sh
+++ nvidia-ubuntu22-selectel.sh
@@ -1,12 +1,14 @@
+# Warning
 echo 'Warning: this script is designed to work with selectel infrastructure only, input anything if you understand'
 read sane
-
+#
 if [ -z "$sane" ]
 then
   echo 'Quit'
   exit 1
 fi
 
+# User input
 echo 'Script will now try loading the scene from storage container(scene must be in the root of the container), input the name(for example "scene_storage")'
 read cont_name
 echo 'Enter the scene filename(*.blend) and other render params'
@@ -19,21 +21,29 @@ echo 'Render engine(CYCLES)'
 read rend_eng
 echo 'Render using(CUDA,OPTIX(+CPU)):'
 read rend_with
-
+#
 if [ -z "$cont_name" ] | [ -z "$scene_name" ] | [ -z "$s_frame" ] | [ -z "$e_frame" ] | [ -z "$rend_with" ]
 then
   echo 'Missing input parameter'
   exit 1
 fi
-
+#
 echo 'Please wait for the aws to install and enter credentials'
 sleep 3
+
+# Basic utils install
 apt-get update -y
 apt-get install -y blender tmux htop awscli
+
+# AWS configuration
 aws configure --endpoint-url=https://s3.storage.selcloud.ru
+
+# Blender 'installation'
 wget https://mirrors.dotsrc.org/blender/release/Blender3.2/blender-3.2.0-linux-x64.tar.xz
 tar -xpvf blender-3.2.0-linux-x64.tar.xz
 rm blender-3.2.0-linux-x64.tar.xz
+
+# Nvidia drivers installation
 add-apt-repository ppa:graphics-drivers/ppa
 apt-get update -y
 apt-get install -y linux-headers-$(uname -r)
@@ -44,10 +54,16 @@ dpkg -i cuda-repo-ubuntu2204-11-7-local_11.7.0-515.43.04-1_amd64.deb
 cp /var/cuda-repo-ubuntu2204-11-7-local/cuda-*-keyring.gpg /usr/share/keyrings/
 apt-get update -y
 apt-get install -y cuda
+
+# Render preparations
 mkdir -p /render/output
 cd /root
 aws --endpoint-url=https://s3.storage.selcloud.ru s3 cp "s3://$cont_name/$scene_name" "$scene_name"
+
+# Render
 /root/blender-3.2.0-linux-x64/blender -b "/root/$scene_name" -E "$rend_eng" -o /render/output/ -noaudio -s "$s_frame" -e "$e_frame" -a -- --cycles-device "$rend_with"
+
+# Uploading the output
 echo 'Output frames will be packed in an archive and uploaded to the container'
 tar -zcvf render_output.tar.gz /render/output/
 aws --endpoint-url=https://s3.storage.selcloud.ru s3 cp render_output.tar.gz "s3://$cont_name/" && echo 'Done'
